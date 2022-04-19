1979a9eac44278474545651bbcdd90eb1be85466
Kealan Parr <kealantparr@gmail.com>
2022-04-19 15:19:11 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var cors = require('cors');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+	res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ "name": req.file.originalname, "type": req.file.mimetype, "size": req.file.size }));
+})
+
+app.get('/', (req, res) => {
+	res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.listen(3000, () => {
+	console.log('Your app is listening on port 3000')
+});
